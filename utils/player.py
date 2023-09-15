bfa6b92aef56c86a84ce3a0b0c1ee2d37f71200b
Alexandre Bove <46528060+bovealexandre@users.noreply.github.com>
2023-09-15 11:35:58 +0200

    interactive gameplay

--- utils/player.py
+++ utils/player.py
@@ -15,15 +15,16 @@ class Player:
         self.turn_count: int = 0
         self.number_of_cards: int = 0
         self.history: list[Card] = []
+        self.points: int = 0
 
     def __str__(self) -> str:
         """Return a string representation of the player's state."""
         return """The player name is {self.name}
-his cards are : {cards}
+his cards are : {self.cards}
 he is on turn : {self.turn_count}
 he has {self.number_of_cards} cards
-here is his history {history}""".format(
-            self=self, cards=self.cards[::], history=self.history
+here is his history {self.history}""".format(
+            self=self
         )
 
     def play(self) -> Card:
@@ -32,9 +33,19 @@ here is his history {history}""".format(
         Returns:
             Card: The card played by the player.
         """
-        played_card: Card = choice(self.cards)
+        print(f"{self.name}, here are your cards : {self.cards}")
+        play_input: int = int(
+            input(
+                f"{self.name}, It's your turn ! What card do you want to play? (this takes an Int) : "
+            )
+        )
+        played_card: Card = self.cards[play_input]
+        # played_card: Card = choice(self.cards)
         print(f"{self.name} {self.turn_count} played: {played_card}")
         self.cards.remove(played_card)
         self.turn_count += 1
         self.number_of_cards -= 1
         return played_card
+
+    def add_point(self) -> None:
+        self.points += 1
